e703c2054c57858aa56ce4e1991e9ec39b2bd896
Elinor <israeli.elinor@gmail.com>
2025-07-19 13:16:18 +0300

    Delete script

--- backend/scripts/test_queries.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv')
-const MongoClient = require('mongodb').MongoClient
-
-dotenv.config()
-
-const client = await MongoClient.connect(process.env.MONGODB_URI)
-db = client.db(config.dbName)
-
-db['jobskill-logs'].find({}).sort({timestamp: -1}).limit(5)
